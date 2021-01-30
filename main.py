1cf7f1c26a903af8d8e2001969a21607b4475e71
Agamendon <gosha20052811@gmail.com>
2021-01-30 13:11:26 +0200

    gitignore, changed libraries

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import telebot
 from bs4 import BeautifulSoup
 import time
+import datetime
 
 
 def get_page(url, sess: requests.Session):
@@ -31,23 +32,36 @@ def get_link(text):
 
 
 if __name__ == "__main__":
-    bot = telebot.TeleBot('1649976187:AAFz6_PCCD3cDKSmbUDY6BZ6PtsA_1Cl1_s')
+
+    TOKEN = '1649976187:AAFz6_PCCD3cDKSmbUDY6BZ6PtsA_1Cl1_s'
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56"}
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56"
+    }
+
+    bot = telebot.TeleBot(TOKEN)
+
     session = requests.Session()
     session.headers.update(headers)
 
-    page_text = get_page('https://matholymp.com.ua/', session)
-    prev_headers = get_headers(page_text)
+    prev_headers = []
 
-    bot.send_message('@matholymp_notifications', 'Сервіс почав роботу')
+    # define prev_headers to avoid repeating of material
+    try:
+        page_text = get_page('https://matholymp.com.ua/', session)
+        prev_headers = get_headers(page_text)
+    except Exception as e:
+        print(e)
+
+    # send me message to ensure that bot is working
+    bot.send_message('551027089', 'Started work on UTC {}'.format(datetime.datetime.now()))
+    print('Started work on UTC {}'.format(datetime.datetime.now()))
 
     while True:
         try:
             page_text = get_page('https://matholymp.com.ua/', session)
             curr_headers = get_headers(page_text)
             if are_headers_same(prev_headers, curr_headers):
-                print('true')
+                print(f'Check on {datetime.datetime.now()}, headers are same')
             else:
                 print('false')
                 bot.send_message('@matholymp_notifications', get_link(str(curr_headers[0])))
